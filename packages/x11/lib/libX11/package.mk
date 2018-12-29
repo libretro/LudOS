32bc7583c1841588d9f5cf309f842b815198ce5f
Jean-André Santoni <jean.andre.santoni@gmail.com>
2018-12-29 16:44:05 +0100

    WIP RPi2 port

--- packages/x11/lib/libX11/package.mk
+++ packages/x11/lib/libX11/package.mk
@@ -30,3 +30,12 @@ PKG_CONFIGURE_OPTS_TARGET="--disable-loadable-i18n \
                            --disable-ipv6 \
                            --without-launchd \
                            --without-lint"
+
+pre_make_target() {
+  echo '' > $PKG_BUILD/src/util/Makefile.am
+  $HOST_CC $PKG_BUILD/src/util/makekeys.c -o $PKG_BUILD/.$TARGET_NAME/src/util/makekeys
+}
+
+make_target() {
+  make V=1 CC=$CC AR=$AR LD=$LD CFLAGS="$TARGET_CFLAGS"
+}
