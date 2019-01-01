4c33321f83dd47e79e31cd5e6acc9403b0682e7c
Jean-André Santoni <jean.andre.santoni@gmail.com>
2019-01-01 08:53:06 +0000

    Expose ludo folders

--- packages/mediacenter/ludo/package.mk
+++ packages/mediacenter/ludo/package.mk
@@ -7,7 +7,7 @@ PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/libretro/ludo"
 PKG_DEPENDS_TARGET="toolchain openal-soft"
 PKG_LONGDESC="A libretro frontend written in golang."
-PKG_VERSION="0.1.17"
+PKG_VERSION="0.1.18"
 PKG_URL="https://github.com/libretro/ludo/releases/download/v$PKG_VERSION/Ludo-Linux-$ARCH-$PKG_VERSION.tar.gz"
 PKG_SOURCE_NAME="Ludo-Linux-$ARCH-$PKG_VERSION.tar.gz"
 PKG_TOOLCHAIN="manual"
@@ -25,8 +25,27 @@ if [ ! "$OPENGL" = "no" ]; then
 fi
 
 makeinstall_target() {
-  mkdir -p $INSTALL/usr/bin/ludo
-    cp -r * $INSTALL/usr/bin/ludo/
+  mkdir -p $INSTALL/usr/bin
+    cp ./ludo $INSTALL/usr/bin/ludo
+  mkdir -p $INSTALL/usr/share/ludo
+    cp -r ./assets $INSTALL/usr/share/ludo/assets
+    cp -r ./database $INSTALL/usr/share/ludo/database
+  mkdir -p $INSTALL/usr/lib/libretro
+    cp -r ./cores/* $INSTALL/usr/lib/libretro
+
+  mkdir -p $INSTALL/etc
+    echo '{
+  "video_fullscreen": true,
+  "cores_dir": "/usr/lib/libretro",
+  "assets_dir": "/usr/share/ludo/assets",
+  "database_dir": "/usr/share/ludo/database",
+  "savestates_dir": "/storage/savestates",
+  "savefiles_dir": "/storage/savefiles",
+  "screenshots_dir": "/storage/screenshots",
+  "system_dir": "/storage/system",
+  "playlists_dir": "/storage/playlists",
+  "thumbnail_dir": "/storage/thumbnails"
+}' > $INSTALL/etc/ludo.json
 }
 
 post_install() {
