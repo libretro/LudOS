84dfef34757359fd506aabef7797237538ae83ca
Jean-André Santoni <jean.andre.santoni@gmail.com>
2019-02-27 23:46:36 +0700

    Add LudOS (#8)

--- packages/x11/lib/libX11/package.mk
+++ packages/x11/lib/libX11/package.mk
@@ -8,7 +8,7 @@ PKG_SHA256="4d3890db2ba225ba8c55ca63c6409c1ebb078a2806de59fb16342768ae63435d"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.x.org/"
 PKG_URL="http://xorg.freedesktop.org/archive/individual/lib/$PKG_NAME-$PKG_VERSION.tar.bz2"
-PKG_DEPENDS_TARGET="toolchain util-macros xtrans libXau libxcb"
+PKG_DEPENDS_TARGET="toolchain util-macros xtrans libXau libxcb xorgproto:host"
 PKG_LONGDESC="LibX11 is the main X11 library containing all the client-side code to access the X11 windowing system."
 PKG_TOOLCHAIN="autotools"
 
@@ -30,3 +30,9 @@ PKG_CONFIGURE_OPTS_TARGET="--disable-loadable-i18n \
                            --disable-ipv6 \
                            --without-launchd \
                            --without-lint"
+
+pre_make_target() {
+  echo '' > $PKG_BUILD/src/util/Makefile.am
+  $HOST_CC $HOST_CFLAGS -I$PKG_BUILD/include $PKG_BUILD/src/util/makekeys.c -o $PKG_BUILD/.$TARGET_NAME/src/util/makekeys
+}
+
