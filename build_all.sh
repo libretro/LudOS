916b38354a49f0061b4ddc72d6300e9dba5be905
Jean-André Santoni <jean.andre.santoni@gmail.com>
2019-04-20 07:19:32 +0000

    Build noobs images

--- build_all.sh
+++ build_all.sh
@@ -6,5 +6,5 @@ export IGNORE_VERSION=1
 export OFFICIAL=yes
 export DISTRO=LudOS
 
-PROJECT=RPi DEVICE=RPi2 ARCH=arm make image
+PROJECT=RPi DEVICE=RPi2 ARCH=arm make noobs
 PROJECT=Generic ARCH=x86_64 make image
