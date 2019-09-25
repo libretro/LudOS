854c2615be4a96663eb1bd07506756fc94e50206
Jean-André Santoni <jean.andre.santoni@gmail.com>
2019-09-25 04:14:43 +0200

    Update Ludo to v0.8.5

--- build_all.sh
+++ build_all.sh
@@ -5,6 +5,7 @@ rm -rf target/
 export IGNORE_VERSION=1
 export OFFICIAL=yes
 export DISTRO=LudOS
+export THREADCOUNT=8
 
 PROJECT=Generic ARCH=x86_64 make image
 PROJECT=RPi DEVICE=RPi2 ARCH=arm make noobs
